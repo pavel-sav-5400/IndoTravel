f45bae8c2be483ef05c794879a5beae55f6563e9
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-05-11 17:27:41 +1000

    добавил функцию init

--- script.js
+++ script.js
@@ -2,9 +2,26 @@
 import {
   start,
 } from './modules/timer.js';
-
+import {
+  menu,
+} from './modules/burgerMenu.js';
+import {
+  accord,
+} from './modules/accord.js';
 export const sklonenie = (number, txt, cases = [2, 0, 1, 1, 1, 2]) =>
   txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ?
   number % 10 : 5]];
 
-start();
+
+const init = () => {
+  const startTimer = start();
+  const mainMenu = menu();
+  const accordion = accord();
+  return {
+    startTimer,
+    mainMenu,
+    accordion,
+  };
+};
+
+init();
