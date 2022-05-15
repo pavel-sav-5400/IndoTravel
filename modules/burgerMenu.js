f7ad8157c24282ec03af14b4696551fa0d9d7bbb
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-05-15 19:09:52 +1000

    добавил анимацию в меню

--- modules/burgerMenu.js
+++ modules/burgerMenu.js
@@ -3,8 +3,17 @@ import {
   headerMenu,
 } from './getElems.js';
 
+const menuAnimation = () => {
+  headerMenu.classList.toggle('header__menu_active');
+  if (headerMenu.classList.contains('header__menu_active')) {
+    headerMenu.style.cssText = `
+  transform: translateY(20%);
+  `;
+  } else {
+    headerMenu.classList.remove('header__menu_active');
+  }
+};
+
 export const menu = () => {
-  headerMenuButton.addEventListener('click', () => {
-    headerMenu.classList.toggle('header__menu_active');
-  });
+  headerMenuButton.addEventListener('click', menuAnimation);
 };
