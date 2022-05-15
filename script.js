e9f0300d63bda9fc4a13cd2201227d97627f52ca
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-05-15 19:11:58 +1000

    1

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ import {
 import {
   accord,
 } from './modules/accord.js';
+import './modules/fly.js';
 export const declination = (number, txt, cases = [2, 0, 1, 1, 1, 2]) =>
   txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ?
   number % 10 : 5]];
