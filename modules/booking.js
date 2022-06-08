ea85ae2592f95f62dadfb1efdc79301361fe6dd3
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-06-08 13:10:14 +1000

    реализовал заполнение выпадающих списков на основе json

--- modules/booking.js
+++ modules/booking.js
@@ -1,7 +1,12 @@
+/* eslint-disable require-jsdoc */
+import {declination} from '../script.js';
 
 const reservDate = document.querySelector('#reservation__date');
 const reservPeople = document.querySelector('#reservation__people');
-const reservOpt = document.querySelectorAll('.reservation__option');
+const tourDate = document.querySelector('#tour__date');
+const tourPeople = document.querySelector('#tour__people');
+const reservationData = document.querySelector('.reservation__data');
+const reservationPrice = document.querySelector('.reservation__price');
 
 const loadData = async () => {
   const result = await fetch('booking.json');
@@ -9,32 +14,101 @@ const loadData = async () => {
   return data;
 };
 
-const addList = async () => {
+function* makeRangeIterator(start = 0, end = 0, step = 1) {
+  let iterationCount = 0;
+  for (let i = start; i < end; i += step) {
+    iterationCount++;
+    yield i;
+  }
+  return iterationCount;
+}
+
+// заполнение дат тура
+const addDateList = async () => {
   const data = await loadData();
-  const dates = data.map((item, index) => {
-    reservOpt[index].innerHTML = `
+  const dates = data.map(item => {
+    reservDate.insertAdjacentHTML('beforeend', `
     <option value="${item.date}" class="tour__option reservation__option">${item.date}</option>  
-    `;
-    return reservOpt;
+    `);
+    tourDate.insertAdjacentHTML('beforeend', `
+    <option value="${item.date}" class="tour__option reservation__option">${item.date}</option>  
+    `);
   });
   reservDate.append(...dates);
+  tourDate.append(...dates);
 };
-addList();
+addDateList();
 
-const getData = async () => {
+// заполнение количества людей в выпадающие списки
+const addPeopleList = data => {
+  const arr = data;
+  const people = arr.map(item => {
+    reservPeople.insertAdjacentHTML('beforeend', `
+    <option value="${item}" class="tour__option reservation__option">${item}</option>  
+    `);
+    tourPeople.insertAdjacentHTML('beforeend', `
+    <option value="${item}" class="tour__option reservation__option">${item}</option>  
+    `);
+  });
+  reservPeople.append(...people);
+  tourPeople.append(...people);
+};
+
+// получаем количество человек в зависимости от даты
+const reservationDateForm = async () => {
   const data = await loadData();
-  reservDate.addEventListener('click', (e) => {
-    const date = document.querySelector('#reservation__date').value;
-    if (date === '12.02 - 26.02') {
-      console.log(data[2].price);
-      console.log(data[2]['min-people']);
-      console.log(data[2]['max-people']);
+  const date = reservDate.value;
+  data.find(item => {
+    if (item.date === date) {
+      const people = makeRangeIterator(item['min-people'], (item['max-people'] + 1));
+      const arrPeople = [];
+      for (const i of people) {
+        arrPeople.push(i);
+      }
+      addPeopleList(arrPeople);
     }
   });
 };
-getData();
 
-reservPeople.addEventListener('click', (e) => {
-  const people = document.querySelector('#reservation__people').value;
-  console.log('people: ', people);
+// форма "узнать цену"
+const tourDateForm = async () => {
+  const data = await loadData();
+  const date = tourDate.value;
+  data.find(item => {
+    if (item.date === date) {
+      const people = makeRangeIterator(item['min-people'], (item['max-people'] + 1));
+      const arrPeople = [];
+      for (const i of people) {
+        arrPeople.push(i);
+      }
+      addPeopleList(arrPeople);
+    }
+  });
+};
+
+// вывод цены на страницу
+const getInfoReserv = async () => {
+  const data = await loadData();
+  const date = reservDate.value;
+  const people = reservPeople.value || 0;
+  data.find(item => {
+    if (item.date === date) {
+      const price = item.price;
+      reservationPrice.innerHTML = people * price;
+      reservationData.innerHTML = `${date}, ${people} 
+      ${declination(people, ['человек', 'человека', 'человек'])}`;
+    }
+  });
+};
+
+reservDate.addEventListener('click', () => {
+  reservationDateForm();
+  getInfoReserv();
 });
+
+tourDate.addEventListener('click', tourDateForm);
+
+reservPeople.addEventListener('click', () => {
+  getInfoReserv();
+});
+
